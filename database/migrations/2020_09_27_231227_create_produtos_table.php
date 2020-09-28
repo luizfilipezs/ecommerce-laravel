ce0326bff5bb8625d07e0c23ab3d506abd2cb13d
luizfilipezs <filipeluiz.bs@gmail.com>
2020-09-27 23:48:41 -0300

    Create model Produto

---
+++ database/migrations/2020_09_27_231227_create_produtos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 128);
+            $table->float('preco');
+            $table->integer('qntd_estoque');
+            $table->string('descricao', 200);
+            $table->string('medidas', 32)->default('');
+            $table->string('foto');
+            $table->boolean('ativo')->default('true');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
